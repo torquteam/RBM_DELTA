353103987b0f362fa23818dd7773fa2e9538ca78
torquteam <50932662+torquteam@users.noreply.github.com>
2024-05-14 16:05:47 -0400

    Add files via upload

---
+++ 16,8/c_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Define the dimension of the problem
+const double conv_r0_en = 0.08405835143769969;
+const double enscale_mev = 13.269584506383948;
+const int num_basis_states_s = 7;
+const int num_basis_states_v = 7;
+const int num_basis_states_b = 7;
+const int num_basis_states_l = 7;
+const int num_basis_states_a = 7;
+
+// Define your multidimensional C function here
+void c_function(double *x, double *y, double* params) {
+    // Example function: f(x, y) = (x^2 - 4, y^2 - 4)
+    double sdensn_s[num_basis_states_s]; double sdensp_s[num_basis_states_s]; double dsdr2_s[num_basis_states_s]; double dsdr_s[num_basis_states_s];
+    double S_s[num_basis_states_s]; double S2_s[num_basis_states_s]; double S3_s[num_basis_states_s];
+    double vdensn_v[num_basis_states_v]; double vdensp_v[num_basis_states_v]; double dvdr2_v[num_basis_states_v]; double dvdr_v[num_basis_states_v];
+    double V_v[num_basis_states_v]; double VB2_v[num_basis_states_v]; double V3_v[num_basis_states_v];
+    double vdensn_b[num_basis_states_b]; double vdensp_b[num_basis_states_b]; double dbdr2_b[num_basis_states_b]; double dbdr_b[num_basis_states_b];
+    double B_b[num_basis_states_b]; double BV2_b[num_basis_states_b];
+    double sdensn_l[num_basis_states_l]; double sdensp_l[num_basis_states_l]; double dldr2_l[num_basis_states_l]; double dldr_l[num_basis_states_l];
+    double L_l[num_basis_states_l];
+    double vdensp_a[num_basis_states_a]; double dadr2_a[num_basis_states_a]; double dadr_a[num_basis_states_a];
+    double a; double b;
+
+
+}
+
+double BA_function(double *x, double* params) {
+    double meson_free, meson_interact, en_BA, BA;
+
+    return BA;
+}
+
+double Rch(double*x) {
+    double Rch, Rp2;
+
+    return Rch;
+}
+
+double Wkskin(double*x) {
+    double FchFwk;
+    FchFwk = -1;
+    return FchFwk;
+}
+
+// Define a function to compute the Jacobian matrix
+void compute_jacobian(double *x, double *jac, double *params) {
+   
+}
