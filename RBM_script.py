435fa4b403acc569354c9d793ee32305b19eda20
Marc Salinas <marcalsalinas@gmail.com>
2024-06-10 19:08:30 -0400

    all greedy files generated

--- RBM_script.py
+++ RBM_script.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     # User Input
     ##################################################################
     #specify the nucleus (6 broken)
-    nucleus = 7
+    nucleus = 0
 
     # Jacobian computation
     jac = False
