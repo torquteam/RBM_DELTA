964a5f252ae96b9ccb63b47b0f97b8f65f7d0686
Marc Salinas <marcalsalinas@gmail.com>
2024-06-11 02:18:12 -0400

    ran greedy algo for nuclei and compiled greedy c file

--- RBM_solve_greedy.py
+++ RBM_solve_greedy.py
@@ -14,7 +14,7 @@ print("Current Working directory:", current_directory)
 
 # Specify the nucleus
 ##################################################################
-nucleus = 5
+nucleus = 0
 ##################################################################
 
 # Specify the number of proton and neutron states
